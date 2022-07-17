612db28b2d7e33d7e77d4b1606ac00b0ed32c458
Ali G.Jahed <aligjahed@gmail.com>
2022-07-17 10:25:54 -0700

    Change folder structure and added download functionality

--- InstaSaveFarsiBot/Config/BotConfiguration.cs
+++ InstaSaveFarsiBot/Config/BotConfiguration.cs
@@ -1,4 +1,4 @@
-﻿namespace InstaSaveFarsiBot.BotConfiguration;
+﻿namespace InstaSaveFarsiBot.Config;
 public class BotConfiguration
 {
     public readonly static string BotToken = Environment.GetEnvironmentVariable("BOT_TOKEN");
